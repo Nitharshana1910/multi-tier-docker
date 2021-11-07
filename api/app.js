df6344426ee10f5799643a578ec4af6706a5c3a6
Mahenthiran <nitharshana@speedcodelabs.com>
2021-11-07 18:56:23 +0530

    Setup body-parser package

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 // const cors = require('cors');
 // const db = require('./config/db');
 
@@ -15,8 +15,8 @@ const PORT = 5000;
 // setup middlewares
 // app.use(cors());
 app.use(morgan('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.json('api is working!!');
